9976ed7561b11de1ef91e537b690b3a9b99484b9
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-03-21 15:32:00 +0000

    f_e and f_o

--- FriFv.lean
+++ FriFv.lean
@@ -8,11 +8,18 @@ import Mathlib.Algebra.Polynomial.FieldDivision
 
 variable {F: Type} [Field F] [Finite F] [DecidableEq F]
 
-noncomputable def divide (f : Polynomial F) (D : Fˣ -> Prop) : Polynomial F :=
+noncomputable def fₑ (f : Polynomial F) (D : Fˣ -> Prop) : Polynomial F :=
   match f.degree with
   | none => 0
   | some n =>
-    let m := (n + 1) / 2
-    let g := f.div (Polynomial.sub.sub (Polynomial.monomial (m : ℕ) 1) 1)
-    let h := f.div (Polynomial.sub.sub (Polynomial.monomial (n - m) 1) 1)
-    g + Polynomial.monomial (n - m) 1 * h
+    let X := Polynomial.X
+    let minusX := -X
+    (Polynomial.mul'.mul (Polynomial.C (2⁻¹ : F)) (f.comp X + f.comp minusX))
+
+noncomputable def fₒ (f : Polynomial F) (D : Fˣ -> Prop) : Polynomial F :=
+  match f.degree with
+  | none => 0
+  | some n =>
+    let X := Polynomial.X
+    let minusX := -X
+    (Polynomial.mul'.mul (Polynomial.C (2⁻¹ : F)) (f.comp X - f.comp minusX)) /ₘ X
